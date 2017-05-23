5179f10bcb54cb4b281ebd772388ee261031db6e
dohertki <kierind@gmail.com>
2017-05-23 09:33:55 -0700

    Move to laptop

--- js/script_params.js
+++ js/script_params.js
@@ -1,12 +1,13 @@
 // *************************************************************************
 var apiKey = '01ddf7484cf95ea7346008867a667fef';
-var t1 = 1;
+var t1 = '1';
 
 var key = '01ddf7484cf95ea7346008867a667fef';
 var privateKey = '6c6d5d8eb234358566e28751b2c644e32f219b54';
-var hash = '145098c50dc22355ee0fa4a50e3866c4';
+var hash = '3bee95fd7a0584916c3332a5f4c5720d';
 //var url = 'http://gateway.marvel.com/v1/public/characters?ts=${t1}1&apikey=${key}&hash=${hash}&limit=5';
-var url = 'http://gateway.marvel.com/v1/public/characters?&apikey=${key}&limit=5';
+
+var url = 'http://gateway.marvel.com/v1/public/comics?ts=1&apikey=01ddf7484cf95ea7346008867a667fef&hash=3bee95fd7a0584916c3332a5f4c5720d';
 
 
 document.addEventListener('DOMContentLoaded', bindButtons);
@@ -20,6 +21,9 @@ document.addEventListener('DOMContentLoaded', bindButtons);
    req.onload = function(){
    	 if(req.status === 200){
    	 	console.log("success");
+   	    debugger;
+
+   	    var report = JSON.parse(req.responseText);
    	 }
    }
   console.log("ready state: " + req.readyState);
